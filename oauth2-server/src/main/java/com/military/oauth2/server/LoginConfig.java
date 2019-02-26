6e888eeed481b1d85ec7f2798ca6cb532acfc4ce
flyer_21 <flyer_21@163.com>
2019-02-26 20:31:17 +0800

    init

--- oauth2-server/src/main/java/com/military/oauth2/server/LoginConfig.java
+++ oauth2-server/src/main/java/com/military/oauth2/server/LoginConfig.java
@@ -1,19 +1,22 @@
 package com.military.oauth2.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+//import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
+
 @Configuration
-@Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
+@Order( 99)
 public class LoginConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private  org.springframework.security.authentication.AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManager;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,6 +26,12 @@ public class LoginConfig extends WebSecurityConfigurerAdapter {
         http.antMatcher("**").formLogin().loginPage("/login").permitAll().and().authorizeRequests()
                 .anyRequest().authenticated();
 
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
